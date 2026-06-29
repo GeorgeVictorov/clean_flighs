d661b04aa3ec78690278c6ffb96a8b5a428c9dfd
georgiiviktorov <nowayisleft@gmail.com>
2026-06-29 17:10:44 +0700

    feat: use cases tests

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -1,15 +1,22 @@
 import pytest
 
-from flights.domain.commands import CreateFlight, ReserveSeat
-from flights.domain.errors import FlightAlreadyExists
+from flights.domain.commands import (CancelReservation, CreateFlight,
+                                     ReserveSeat)
+from flights.domain.errors import FlightAlreadyExists, FlightNotFound
+from flights.infrastructure.repo.memory.memory_repository import \
+    InMemoryRepository
 from flights.infrastructure.uow.memory.memory_uow import InMemoryUnitOfWork
-from flights.infrastructure.repo.memory.memory_repository import InMemoryRepository
 from flights.service_layer.service import FlightService
 
 
-def create_unit_of_work():
-    uow = InMemoryUnitOfWork(InMemoryRepository())
-    return uow
+@pytest.fixture
+def uow():
+    return InMemoryUnitOfWork(InMemoryRepository())
+
+
+@pytest.fixture
+def service(uow):
+    return FlightService(uow)
 
 
 def create_command_create_flight():
@@ -29,18 +36,23 @@ def create_command_reserve_seat():
     return cmd
 
 
-def test_create_flight_success():
-    uow = create_unit_of_work()
-    service = FlightService(uow)
+def create_command_cancel_reservation():
+    cmd = CancelReservation(
+        flight_id="SU-901",
+        passenger_id="p01",
+    )
+
+    return cmd
+
+
+def test_create_flight_success(service, uow):
     cmd = create_command_create_flight()
     service.create_flight(cmd)
 
     assert uow.flights.get(flight_id="SU-901").flight_id == cmd.flight_id
 
 
-def test_create_flight_already_exists_failure():
-    uow = create_unit_of_work()
-    service = FlightService(uow)
+def test_create_flight_already_exists_failure(service):
     cmd = create_command_create_flight()
     service.create_flight(cmd)
 
@@ -48,10 +60,7 @@ def test_create_flight_already_exists_failure():
         service.create_flight(cmd)
 
 
-def test_reserve_seat_success():
-    uow = create_unit_of_work()
-    service = FlightService(uow)
-
+def test_reserve_seat_success(service, uow):
     cmd = create_command_create_flight()
     service.create_flight(cmd)
 
@@ -64,3 +73,41 @@ def test_reserve_seat_success():
 
     assert seat.passenger_id == cmd.passenger_id
     assert seat.is_reserved
+
+
+def test_reserve_seat_flight_not_found_failure(service):
+    cmd = create_command_create_flight()
+    service.create_flight(cmd)
+
+    with pytest.raises(FlightNotFound):
+        service.reserve_seat(ReserveSeat(
+            flight_id="SU-902",
+            passenger_id="p01",
+            seat_id="A1"
+        ))
+
+
+def test_cancel_reservation_success(service, uow):
+    cmd = create_command_create_flight()
+    service.create_flight(cmd)
+
+    cmd = create_command_reserve_seat()
+    service.reserve_seat(cmd)
+
+    cmd = create_command_cancel_reservation()
+    service.cancel_reservation(cmd)
+
+    flight = uow.flights.get(flight_id="SU-901")
+
+    seat = next(s for s in flight.seats if s.seat_id == "A1")
+
+    assert seat.passenger_id is None
+    assert seat.is_reserved is False
+
+
+def test_cancel_reservation_flight_not_found_failure(service):
+    with pytest.raises(FlightNotFound):
+        service.cancel_reservation(CancelReservation(
+            flight_id="SU-902",
+            passenger_id="p01",
+        ))
